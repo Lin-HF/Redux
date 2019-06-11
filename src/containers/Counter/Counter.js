2f9df5f1a0889f02010fd03a0129d4859e8f3a58
David Lin <haiflin@iu.edu>
2019-06-11 19:42:39 -0400

    pass date with action

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -6,35 +6,35 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import {connect} from 'react-redux';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+    // state = {
+    //     counter: 0
+    // }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
+    // counterChangedHandler = ( action, value ) => {
+    //     switch ( action ) {
+    //         case 'inc':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+    //             break;
+    //         case 'dec':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+    //             break;
+    //         case 'add':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+    //             break;
+    //         case 'sub':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+    //             break;
+    //     }
+    // }
 
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAdd5Counter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubstract5Counter}  />
             </div>
         );
     }
@@ -49,7 +49,11 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: "INCREMENT"})
+        onIncrementCounter: () => dispatch({type: "INCREMENT"}),
+        onDecrementCounter: () => dispatch({type: "DECREMENT"}),
+        onAdd5Counter: () => dispatch({type: "ADD5", val: 5}),
+        onSubstract5Counter: () => dispatch({type: "SUBSTRACT5", val: 5})
+
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,21 @@ const reducer = (state = initialStore, action) => {
             counter: state.counter + 1
         }
     }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD5') {
+        return {
+            counter: state.counter + action.val
+        }
+    }
+    if (action.type === 'SUBSTRACT5') {
+        return {
+            counter: state.counter - action.val
+        }
+    }
     return state;
 }
 
